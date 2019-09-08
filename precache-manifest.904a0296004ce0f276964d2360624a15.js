68d97ff1c40107fcbe243b758aa086ebcd7ae6e2
Jeferson <jeferson.silva@rakuten.com>
2019-09-08 22:38:41 +0200

    Updates

--- precache-manifest.904a0296004ce0f276964d2360624a15.js
+++ precache-manifest.904a0296004ce0f276964d2360624a15.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e50c1834e9248f883c1208f71fd04251",
+    "revision": "32eb775cde765a2ceb46289b8a93671a",
     "url": "/hips-web/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/hips-web/static/css/2.cf291e2b.chunk.css"
   },
   {
-    "revision": "aa8f6fde2c70c28300b5",
+    "revision": "58d0dc74687d9d9aba13",
     "url": "/hips-web/static/css/main.f41457d1.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/hips-web/static/js/2.a8e77c70.chunk.js"
   },
   {
-    "revision": "aa8f6fde2c70c28300b5",
-    "url": "/hips-web/static/js/main.9ff2f2a6.chunk.js"
+    "revision": "58d0dc74687d9d9aba13",
+    "url": "/hips-web/static/js/main.7458cc1e.chunk.js"
   },
   {
     "revision": "43a86cad8ebb789bc013",
