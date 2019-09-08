0c2f44e61f5ed17879eb2d96d562614bc9fe7454
Jeferson <jeferson.silva@rakuten.com>
2019-09-08 21:16:54 +0200

    Updates

---
+++ precache-manifest.f8ada2782d2778ab2778739a2d7ee71d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "869a7dc887a4055f16df8c0291db8797",
+    "url": "/hips-web/index.html"
+  },
+  {
+    "revision": "ecabf3c1763c95800ea8",
+    "url": "/hips-web/static/css/2.cf291e2b.chunk.css"
+  },
+  {
+    "revision": "1dc243e6bcb20e654461",
+    "url": "/hips-web/static/css/main.f41457d1.chunk.css"
+  },
+  {
+    "revision": "ecabf3c1763c95800ea8",
+    "url": "/hips-web/static/js/2.db6fb4a3.chunk.js"
+  },
+  {
+    "revision": "1dc243e6bcb20e654461",
+    "url": "/hips-web/static/js/main.7e08799c.chunk.js"
+  },
+  {
+    "revision": "43a86cad8ebb789bc013",
+    "url": "/hips-web/static/js/runtime~main.5dd32d5f.js"
+  }
+]);
